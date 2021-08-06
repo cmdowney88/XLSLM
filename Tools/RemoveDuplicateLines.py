9ae82cf9c76a466e5d7852dd8fc43b2de592d794
shannondrizin <47286219+shannondrizin@users.noreply.github.com>
2021-08-05 22:50:03 -0400

    Add scripts to Tools

---
+++ Tools/RemoveDuplicateLines.py
@@ -0,0 +1,117 @@
+"""
+Script to remove any duplicate lines between AmericasNLP files and KannEtAl2018 files for
+the same language. If a line exists in both the AmericasNLP data and KannEtAl2018 data, it is 
+removed from KannEtAl2018 and stays in AmericasNLP. The script is only meant for one language
+at a time.
+Expected command line args: <program_name> <AmericasNLP_train_file> <AmericasNLP_dev_file>
+<KannEtAl2018_file> 
+"""
+import sys
+from typing import List
+
+def remove(
+    anlp_lines: List[str],
+    kea_lines: List[str]
+) -> List[str]:
+    """
+    Removes duplicate lines between AmericasNLP data and Kann et al. 2018 data from the Kann
+    et al. 2018 data.
+    Returns the remaining lines for the Kann et al. 2018 data.
+    Args:
+        anlp_lines: A list of the lines in the AmericasNLP files
+        kea_lines: A list of the lines in the Kann et al. file.
+    Returns:
+        A list of strings, which are the remaining lines from the Kann et al. file after
+        removing duplicates
+    """
+    #to store anlp lines with no whitespace or punctuation
+    stripped_anlp_lines = []
+
+    #remove whitespace and punctuation for anlp lines for comparison with kae lines
+    for line in anlp_lines:
+        stripped_line = no_whitespace_or_punct(line)
+        stripped_anlp_lines.append(stripped_line)
+
+    #to store the final lines from Kann et al. 2018 remaining after removing any duplicates
+    final_kea_lines = []
+
+    #remove whitespace and punctuation for Kann et al. lines and compare with the lines
+    #from AmericasNLP with whitespace and punctuation removed. If not a duplicate line, add
+    #Kann et al. line to list of final lines.
+    for line in kea_lines:
+        orig_line = line
+        stripped_line = no_whitespace_or_punct(line)
+        if stripped_line not in stripped_anlp_lines:
+            final_kea_lines.append(orig_line)
+        
+    return final_kea_lines
+
+
+def no_whitespace_or_punct(line: str) -> str:
+    """
+    Remove whitespace and punctuation from string.
+    Returns a string with whitespace and punctuation removed.
+    Args:
+        line: String to remove whitespace and punctuation from
+    Returns:
+        A string with the whitespace and punctuation removed
+    """
+    #remove whitespace
+    newstr = line.replace(' ','')
+
+    #remove punctuation besides "'" and "+"
+    newstr = newstr.replace('.','').replace('?','').replace('!','').replace(',','')
+    newstr = newstr.replace('*','').replace('%','').replace('$','').replace('=','')
+    newstr = newstr.replace('\\','').replace('/','').replace('[','').replace(']','')
+    newstr = newstr.replace('(','').replace(')','').replace('"','').replace(';','')
+
+    return newstr
+
+def write_to_file(
+    filename: str,
+    prepared_data: List[str]
+):
+    """
+    Write lines from a list of strings to a file. Add a newline after each string.
+    Args:
+        filename: The name of the file to write to
+        prepared_data: The list of strings to write to the file
+    """
+    f = open(filename, 'w')
+    for line in prepared_data:
+        s = line + '\n'
+        f.write(s)
+    f.close()
+
+
+def main():
+    """
+    Main function of program. Gets files from command lines. Removes duplicate lines between
+    AmericasNLP and Kann et al. 2018 from Kann et al. 2018 with remove() and writes the remaining
+    lines to a file with write_to_file().
+    """
+    #Get file paths from command line
+    anlp_train_file = sys.argv[1]
+    anlp_dev_file = sys.argv[2]
+    kea_file = sys.argv[3]
+
+    #get lines from AmericasNLP files
+    anlp_train_lines = [line.strip() for line in open(anlp_train_file, 'r')]
+    anlp_train_lines[:] = [x for x in anlp_train_lines if x]
+
+    anlp_dev_lines = [line.strip() for line in open(anlp_dev_file, 'r')]
+    anlp_dev_lines[:] = [x for x in anlp_dev_lines if x]
+
+    #combine lines from both AmericasNLP files into one list
+    anlp_lines = anlp_train_lines + anlp_dev_lines
+
+    #get lines from Kann et al. 2018 file
+    kea_lines = [line.strip() for line in open(kea_file, 'r')]
+    kea_lines[:] = [x for x in kea_lines if x]
+
+    #Remove duplicate lines and write remaining lines to file
+    final_kea_lines = remove(anlp_lines, kea_lines)
+    write_to_file(kea_file[:-4] + '_removed' + kea_file[-4:], final_kea_lines)
+
+if __name__ == "__main__":
+    main()
---
+++ Tools/stats.py
@@ -0,0 +1,168 @@
+"""
+Script to get stats on the data files in the XLSLM repository.
+Command line arguments are the paths of the data files to get the stats for.
+For each file, the script produces stats for the total number of lines, total number of tokens,
+number of unique tokens, total number of characters, number of characters in unique tokens,
+total number of unique characters, average length of all tokens, and average length of unique
+tokens.
+Each of the data files will have its stats output to a separate file.
+There will also be a combined stats file with the stats for all of the files 
+combined into one stats file, where the stats are separated by the original data file names.
+"""
+import sys
+from typing import Dict, List
+
+def get_stats(filename: str) -> Dict[str, str]:
+    """
+    Get stats for the data files from the XLSLM repository.
+    For the given file, finds the total number of lines, total number of tokens, number 
+    of unique tokens, total number of characters, number of characters in unique tokens,
+    total number of unique characters, average length of all tokens, and average length 
+    of unique tokens.
+    Returns a dict of label, stat pairs that are both represented as strings.
+    Args:
+        filename: The path of the file to find stats for
+    Returns:
+        A dict of key, value pairs that are both strings. The keys are the labels for the stats.
+        The values are the string representations of the stats.
+    """
+    #dictionary to store stats on data
+    stats_dict = {'Filename':filename}
+
+    #open file and get lines
+    lines = [line.strip() for line in open(filename, 'r')]
+    lines[:] = [x for x in lines if x]
+
+    #find how many lines are in file
+    num_lines = len(lines)
+    stats_dict['Number of lines'] = str(num_lines)
+
+    #find total number of tokens
+    total_tokens_list = []
+    for line in lines:
+        tokens = line.split()
+        for token in tokens:
+            total_tokens_list.append(token)
+
+    total_tokens_num = len(total_tokens_list)
+    stats_dict['Total number of tokens'] = str(total_tokens_num)
+    
+    #find total number of unique tokens
+    unique_tokens_list = list(set(total_tokens_list))
+    unique_tokens_num = len(unique_tokens_list)
+    stats_dict['Number of unique tokens'] = str(unique_tokens_num)
+
+    #find total number of characters
+    total_character_num_of_all_tokens = 0
+    for token in total_tokens_list:
+        total_character_num_of_all_tokens += len(token)
+    stats_dict['Total number of characters in file'] = str(total_character_num_of_all_tokens)
+
+    #fine total number of characters in unique tokens
+    total_character_num_of_unique_tokens = 0
+    for token in unique_tokens_list:
+        total_character_num_of_unique_tokens += len(token)
+    stats_dict['Total number of characters in unique tokens'] = str(total_character_num_of_unique_tokens)
+
+    #find unique characters and total number of unique characters
+    #total_chars_list = [char for token in total_tokens_list for char in token]
+    total_chars_list = []
+    for token in total_tokens_list:
+        for char in token:
+                total_chars_list.append(char)
+    total_chars_list[:] = [char.lower() for char in total_chars_list]
+
+    unique_chars_list = list(set(total_chars_list))
+    unique_chars_num = len(unique_chars_list)
+    stats_dict['Total number of unique characters'] = str(unique_chars_num)
+
+    #find average length of all tokens
+    total, counter = 0, 0
+    for token in total_tokens_list:
+        total += len(token)
+        counter += 1
+    avg_token_length = total/counter
+    stats_dict['Average token length of all tokens in file'] = str(avg_token_length)
+
+    #find average length of unique tokens
+    total, counter = 0, 0
+    for token in unique_tokens_list:
+        total += len(token)
+        counter += 1
+    avg_unique_token_length = total/counter
+    stats_dict['Average token length of all unique tokens in file'] = str(avg_unique_token_length)
+
+    return stats_dict
+
+
+def write_to_file(
+    filename: str,
+    stats_dict: Dict[str, str]
+):
+    """
+    Write lines to a file from a dictionary of string keys and string values.
+    Args:
+        filename: The name of the file to write to
+        stats_dict: The dictionary of string keys and values to write to the file
+    """
+    f = open(filename, 'w')
+    for k,v in stats_dict.items():
+        s = str(k) + ': ' + str(v) + '\n'
+        f.write(s)
+    f.close()
+
+
+def write_all_to_file(
+    filename: str,
+    list_of_stats_dicts: List[Dict[str, str]]
+):
+    """
+    Write lines to a file from a list of dictionaires with string keys and string values.
+    For each dictionary in the list, prints each key, value pair to a line.
+    Args:
+        filename: The name of the file to write to
+        list_of_stats_dicts: The list of dictionaries with string keys and values to write
+            to the file.
+    """
+    #overwrite old output file if it exists
+    f = open(filename, 'w')
+    f.close()
+
+    #print to output file
+    for stats_dict in list_of_stats_dicts:
+        f= open(filename, 'a')
+        for k,v in stats_dict.items():
+            s = str(k) + ': ' + str(v) + '\n'
+            f.write(s)
+        f.write('\n')
+        f.close()
+
+
+def main():
+    """
+    Main function of the program. Gets the names of files to find stats for from the command
+    line arguments. For each file, it finds the stats with get_stats() and writes them to 
+    a file with write_to_file(). Also writes stats from all files to combined stats file with 
+    write_all_to_file(). 
+    """
+    #get names of files to find stats for
+    list_of_files = sys.argv[1:]
+    list_of_files.sort()
+
+    #list to store all of the stats dicts to output to combined file
+    list_of_stats = []
+
+    #get stats for each file and write stats to file
+    for filename in list_of_files:
+        stats_dict = get_stats(filename)
+        stats_file = filename[:-4]
+        stats_file_extension = filename[-3:]
+        write_to_file(stats_file + '_stats.' + stats_file_extension , stats_dict)
+        list_of_stats.append(stats_dict)
+
+    #write to combined stats file
+    write_all_to_file('combined_stats.txt', list_of_stats)
+
+
+if __name__ == "__main__":
+    main()
